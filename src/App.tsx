d5e56a9dff9d841a59af40c67ea67800c7365432
ryan woods <federer5050@gmail.com>
2023-03-24 23:29:10 +0000

    starter file

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,27 @@ import React from 'react';
 function App() {
   return (
     <h2>title</h2>
+    //   const myArray = [
+    //     'polish', 'french', 'spanish', 'russian'
+    // ]
+
+
+    //     const [inputText, setInputText] = useState<string>("polish");
+
+
+    //     const filteredItem = myArray.filter(arrayItem => arrayItem.match(inputText))
+    //     console.log(filteredItem);
+
+
+    // Render out JSX below
+    //    <>
+    //   <label>
+    //                                     Name:
+    //                                 <input type="text" name="name" value={inputText} onChange={(e) => setInputText(e.target.value)} />
+    //                                 </label>
+    //    <h2>{inputText === "" ? 5 : filteredItem}</h2>
+    //  </>
+
   );
 }
 
