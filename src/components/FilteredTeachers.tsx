869733ba5306e87fbcebafe8258d5164fc9f07f2
ryan woods <federer5050@gmail.com>
2023-04-06 13:09:04 +0100

    amending price filter so it is possible to lower maximum amount

--- src/components/FilteredTeachers.tsx
+++ src/components/FilteredTeachers.tsx
@@ -11,8 +11,8 @@ interface Props {
 
 const FilteredTeachers: React.FC<Props> = ({ initialInputText }) => {
     const [inputText, setInputText] = useState<string | number>(initialInputText);
-    const [inputPrice, setInputPrice] = useState<number>(0);
     const [maxPrice, setMaxPrice] = useState<number>(50);
+    // const [maxPrice, setMaxPrice] = useState<number>(50);
 
 
     // filter the results by language searched for
@@ -23,31 +23,32 @@ const FilteredTeachers: React.FC<Props> = ({ initialInputText }) => {
 
     // filter the results by price by assinging 'filteredTeachers' to a new array
     filteredTeachers = filteredTeachers.filter(
-        (arrayMatched) => arrayMatched.price >= inputPrice && arrayMatched.price <= maxPrice
+        (arrayMatched) => arrayMatched.price < maxPrice
     );
 
 
     // remove price filter
     const removePriceFilter = () => {
-        setInputPrice(0);
         setMaxPrice(50);
+        // setMaxPrice(50);
     };
 
 
     // this function is called when user selects a price range
     const handlePriceChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setInputPrice(parseInt(event.target.value));
+        setMaxPrice(parseInt(event.target.value));
+        // setMaxPrice(parseInt(event.target.value));
     };
 
     return (
         <>
             <div className={PriceFilterStyles.alignPrice}>
-                <span>Filter by price: {inputPrice} - {maxPrice}</span>
+                <span>Max price: {maxPrice}</span>
                 <div className={PriceFilterStyles.resetPrice} onClick={removePriceFilter}>
                     <svg height="9" viewBox="0 0 12 12" width="9" xmlns="http://www.w3.org/2000/svg" className={PriceFilterStyles.resetPriceCross}><path d="M6 4.586L10.293.293l1.414 1.414L7.414 6l4.293 4.293-1.414 1.414L6 7.414l-4.293 4.293-1.414-1.414L4.586 6 .293 1.707 1.707.293z"></path></svg>
                 </div>
             </div>
-            <input type="range" id="priceRange" name="priceRange" min="0" max="50" step="5" value={inputPrice} onChange={handlePriceChange} className={PriceFilterStyles.priceSlider} />
+            <input type="range" id="priceRange" name="priceRange" min="0" max="50" step="5" value={maxPrice} onChange={handlePriceChange} className={PriceFilterStyles.priceSlider} />
             {/* filter the results by language */}
             <SearchLanguage inputText={inputText} setInputText={setInputText} filteredTeachers={filteredTeachers} />
             {filteredTeachers.map((arrayItem) => {
