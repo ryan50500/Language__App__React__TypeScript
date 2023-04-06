ea6bdbd18d3b0bba9ae9f731433def6155c62fff
ryan woods <federer5050@gmail.com>
2023-04-06 18:08:33 +0100

    removing teacher filter by birth country

--- src/components/FilteredTeachers.tsx
+++ src/components/FilteredTeachers.tsx
@@ -12,7 +12,7 @@ interface Props {
 const FilteredTeachers: React.FC<Props> = ({ initialInputText }) => {
     const [inputText, setInputText] = useState<string | number>(initialInputText);
     const [maxPrice, setMaxPrice] = useState<number>(50);
-    // const [maxPrice, setMaxPrice] = useState<number>(50);
+    const [teacherBirthCountry, setTeacherBirthCountry] = useState<string>("");
 
 
     // filter the results by language searched for
@@ -21,9 +21,9 @@ const FilteredTeachers: React.FC<Props> = ({ initialInputText }) => {
             arrayMatched.language.toLowerCase().indexOf(inputText.toString()) !== -1
     );
 
-    // filter the results by price by assinging 'filteredTeachers' to a new array
+    // filter the results by price and birth country of teacher
     filteredTeachers = filteredTeachers.filter(
-        (arrayMatched) => arrayMatched.price < maxPrice
+        (arrayMatched) => arrayMatched.price < maxPrice && arrayMatched.country.toLowerCase().indexOf(teacherBirthCountry.toString()) !== -1
     );
 
 
@@ -40,6 +40,20 @@ const FilteredTeachers: React.FC<Props> = ({ initialInputText }) => {
         // setMaxPrice(parseInt(event.target.value));
     };
 
+
+    // filter teachers by birth country
+    const handleBirthCountry = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setTeacherBirthCountry(event.target.value);
+        // setMaxPrice(parseInt(event.target.value));
+    };
+
+
+    // remove teacher birth country filter
+    const removeBirthCountry = () => {
+        setTeacherBirthCountry("")
+    };
+
+
     return (
         <>
             <div className={PriceFilterStyles.alignPrice}>
@@ -48,9 +62,13 @@ const FilteredTeachers: React.FC<Props> = ({ initialInputText }) => {
                     <svg height="9" viewBox="0 0 12 12" width="9" xmlns="http://www.w3.org/2000/svg" className={PriceFilterStyles.resetPriceCross}><path d="M6 4.586L10.293.293l1.414 1.414L7.414 6l4.293 4.293-1.414 1.414L6 7.414l-4.293 4.293-1.414-1.414L4.586 6 .293 1.707 1.707.293z"></path></svg>
                 </div>
             </div>
+            {/* filter teachers by price */}
             <input type="range" id="priceRange" name="priceRange" min="0" max="50" step="5" value={maxPrice} onChange={handlePriceChange} className={PriceFilterStyles.priceSlider} />
             {/* filter the results by language */}
             <SearchLanguage inputText={inputText} setInputText={setInputText} filteredTeachers={filteredTeachers} />
+            {/* filter teachers by birth country */}
+            <span>Filter teacher by birth country</span><button onClick={removeBirthCountry}>Remove birth country</button>
+            <input type="text" id="teacherBirth" name="teacherBirth" value={teacherBirthCountry} onChange={handleBirthCountry} />
             {filteredTeachers.map((arrayItem) => {
                 return (
                     <TeacherBorder key={arrayItem.id}>
